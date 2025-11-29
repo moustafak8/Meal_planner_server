34c458f8407e08fcf18ea29ebfbfdf1ca80255e6
moustafak8 <moustafaalkamel@gmail.com>
2025-11-29 19:33:44 +0200

    added household and members controllers and models

--- routes/api.php
+++ routes/api.php
@@ -4,23 +4,21 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\User\TaskController;
-use App\Http\Controllers\Admin\TaskController as TaskAdminController;
+use App\Http\Controllers\User\householdcontroller;
+use App\Models\House_members;
 
 //Authenticated Routes
-/*
+
 Route::group(["prefix" => "v0.1", "middleware" => "auth:api"], function(){
     Route::group(["prefix" => "user"], function(){
-        Route::get('/tasks/{id?}', [TaskController::class, "getAllTasks"])->name("task-listing");
-        Route::post('/add_update_task/{id?}', [TaskController::class, "addOrUpdateTask"]);
-    });
+        Route::get('/household/{id?}', [householdcontroller::class, "getAllhouseholds"]);
+        Route::post('/add_update_household/{id?}', [householdcontroller::class, "addOrUpdatehousehold"]);
+          Route::get('/members/{id?}', [House_members::class, "getAllmembers"]);
+        Route::post('/add_update_household/{id?}', [House_members::class, "addOrUpdatemembers"]);
 
-    //Authenticated Routes
-    Route::group(["prefix" => "admin"], function(){
-        Route::post('/delete_tasks', [TaskAdminController::class, "deleteAllTasks"]);
     });
 });
-*/
+
 
 Route::post('/login', [AuthController::class, "login"]);
 Route::post('/register', [AuthController::class, "register"]);
