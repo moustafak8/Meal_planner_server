9ae905cc9a60cd97960a86b6543f220bef0ba0c7
moustafak8 <moustafaalkamel@gmail.com>
2025-11-29 15:25:44 +0200

    added meals_plan  table

---
+++ database/migrations/2025_11_29_132023_createmeal_plans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('meal_plans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('household_id')->constrained('households')->onDelete('cascade');
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade');
+            $table->string('name');
+            $table->date('start_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
