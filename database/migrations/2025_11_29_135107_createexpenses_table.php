2355dd89d2d47ffef1ee2c319a5ff7b75265997f
moustafak8 <moustafaalkamel@gmail.com>
2025-11-29 15:59:17 +0200

    added expenses table

---
+++ database/migrations/2025_11_29_135107_createexpenses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+       Schema::create('expenses', function (Blueprint $table) {
+           $table->id();
+           $table->foreignId('household_id')->constrained('households')->onDelete('cascade');
+           $table->foreignId('created_by')->constrained('users')->onDelete('cascade');
+           $table->string('title');
+           $table->decimal('amount_spent', 10, 2);
+           $table->string('Currency');
+           $table->string('category');
+           $table->string('notes')->nullable();
+           $table->date('expense_date');
+           $table->timestamps();
+       });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
