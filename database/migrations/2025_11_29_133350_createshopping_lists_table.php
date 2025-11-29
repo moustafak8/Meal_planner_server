d0b369dae13c3a026d90f693d16e2b0585644dab
moustafak8 <moustafaalkamel@gmail.com>
2025-11-29 15:50:41 +0200

    added shopping_list table

---
+++ database/migrations/2025_11_29_133350_createshopping_lists_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shopping_lists', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('household_id')->constrained('households')->onDelete('cascade');
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('meal_plan_id')->constrained('meal_plans')->onDelete('cascade')->nullable();
+            $table->string('name');
+            $table->integer('cost');
+            $table->enum('status', ['pending', 'completed'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
